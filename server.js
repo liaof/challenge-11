5e577493c014b82396cd3b7f389d0a27d2607a54
liaof <f.liao163@gmail.com>
2021-05-09 22:31:18 -0400

    post to .json

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require ('express');
 const fs = require('fs');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -14,15 +15,37 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/api/index',(req,res)=>{
-    res.send(notes);
+    res.json(notes);
     console.log(notes);
 });
 //app.use('/api', apiRoutes);
 app.get('/api/notes', (req,res) =>{
-    res.send(notes);
+    res.json(notes);
     console.log("HELLO");
     console.log(notes);
 });
+
+
+
+app.post('/api/notes', (req, res) => {
+    console.log(req.body);
+    console.log(notes);
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+});
+
+function createNewNote( body,notesArr){
+    console.log(body);
+    const note = body;
+    notesArr.push(note);
+    fs.writeFileSync(
+        path.join(__dirname,'./db/db.json'),
+        JSON.stringify({notes: notesArr }, null, 2)
+    );
+    return note;
+};
+
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
