b2aff0da865e1c2f8b2cd6dd5e0848fcb2613f75
liaof <f.liao163@gmail.com>
2021-05-09 20:52:55 -0400

    get /api/notes and /api/index

--- server.js
+++ server.js
@@ -4,12 +4,25 @@ const fs = require('fs');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+//const {} = require(server end notes script);
+const {notes} = require('./db/db.json');
+
 //JSON parsing middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('public'));
 
+app.get('/api/index',(req,res)=>{
+    res.send(notes);
+    console.log(notes);
+});
+//app.use('/api', apiRoutes);
+app.get('/api/notes', (req,res) =>{
+    res.send(notes);
+    console.log("HELLO");
+    console.log(notes);
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
